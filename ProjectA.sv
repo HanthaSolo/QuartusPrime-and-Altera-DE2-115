e7ef9a4a90a38b6d3d74ebfab9da44d364f9a665
Hantha N <51133574+HanthaSolo@users.noreply.github.com>
2020-01-30 11:31:18 -0800

    Add files via upload

---
+++ ProjectA.sv
@@ -0,0 +1,32 @@
+/**
+ * ClockToNHz.sv
+ * TCES 330 - Spring 2019
+ * Team: Ryan Mateo, Hantha Nyunt, James Stevens
+ * Instructor: Jie Sheng
+ * Project A - Top Level Module
+*/
+
+module ProjectA(SW, LEDG, LEDR, CLOCK_50);
+
+	// Initializing variables
+	input logic CLOCK_50;
+	input [2:0] SW;
+	
+	output [6:0]LEDG;
+	output [3:0] LEDR;
+	
+	logic [2:0] LL,RL;
+	logic OneHzClk;
+	
+	assign LEDR = SW;
+	assign H = SW[2];
+	assign L = SW[1];
+	assign R = SW[0];
+	assign LEDG[6:4] = LL;
+	assign LEDG[2:0] = RL;
+	
+	// Calling function that converts input 50mhz to 1hz
+	ClockToNHz clk1(OneHzClk, CLOCK_50);
+	TaillightsFSM bird(OneHzClk, H, L, R, LL, RL);
+	
+endmodule 
